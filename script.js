5d133ef2da2e367b491e674bbcfeb3534615e46e
RyC2014 <rclingler1986@gmail.com>
2022-07-11 09:47:22 -0400

    added numeric and lower case variables

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ var specialCharacters = [
   '@',  '%',  '+',  '\\',  '/',  "'",  '!',  '#',  '$',  '^',  '?',  ':',  ',',  ')',  '(',  '}',  '{',  ']',  '[',  '~',  '-',  '_',  '.'
 ];
 
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var lowerCasedCharacters = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+];
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
